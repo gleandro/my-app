f848ee1f455e991440be52775a46976926ce3ccd
Giancarlo leandro <giancarlosleandro1@gmail.com>
2022-12-27 13:50:07 -0500

    Anecdotes exercise

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,53 +1,54 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Button = ({ name, onClick }) => (
-  <button onClick={onClick}>{name}</button>
-)
 
-const Statistics = ({ good, regular, bad }) => {
-  let total = good + regular + bad
-  let positive = good / total * 100
-  let salida = "No existen datos"
+const points = [0, 0, 0, 0, 0, 0]
+console.log(points)
+
+const nextAnectdote = (setSelected) => {
+  let number = Math.floor(Math.random() * anecdotes.length)
+  setSelected(number)
+}
+
+const voteAnecdote = (index, setFav) => {
+  points[index] += 1
+  const max_val = Math.max(...points);
+  const max_index = points.indexOf(max_val);
+  setFav(max_index)
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [fav, setFav] = useState(0)
 
   return (
-    total === 0 ? salida :
+    <>
       <div>
-        <table>
-          <tbody>
-            <tr><td>good</td><td> {good}</td></tr>
-            <tr><td>regular</td><td> {regular}</td></tr>
-            <tr><td>bad</td><td> {bad}</td></tr>
-            <tr><td>all</td><td> {total}</td></tr>
-            <tr><td>positive</td><td> {positive}</td></tr>
-          </tbody>
-        </table>
+        <h3>anecdote of the day</h3>
+        {props.anecdotes[selected]}
       </div>
+      <div>
+        <button onClick={() => voteAnecdote(selected, setFav)}>vote</button>
+        <button onClick={() => nextAnectdote(setSelected)}>next anecdote</button>
+      </div>
+      <div>
+        <h3>anecdote withe most votes</h3>
+        {props.anecdotes[fav]}
+      </div>
+    </>
   )
 }
-const App = () => {
 
-  const [good, setGood] = useState(0)
-  const [regular, setRegular] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const setValue = (func, val) => {
-    func(val + 1)
-  }
-
-  return (
-    <div>
-      <h1>Give Feedback</h1>
-      <Button name="bueno" onClick={() => setValue(setGood, good)} />
-      <Button name="neutral" onClick={() => setValue(setRegular, regular)} />
-      <Button name="bad" onClick={() => setValue(setBad, bad)} />
-      <h2>Stadistics</h2>
-      <Statistics good={good} regular={regular} bad={bad} />
-    </div>
-  )
-}
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <App anecdotes={anecdotes} />
 );
 
