7b337f0a7aa30c879f43365d865c444ca93d5d51
Giancarlo leandro <giancarlosleandro1@gmail.com>
2022-12-27 22:42:58 -0500

    Courses

--- homework/course/src/index.js
+++ homework/course/src/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Header = ({ tittle }) => (<h1>{tittle}</h1>)
+
+const Content = ({ parts }) => {
+
+  let total = parts.reduce((s, p) => s + p.exercises, 0)
+  return (
+    <div>
+      {parts.map(x => <Part key={x.id} part={x} />)}
+      <b>Total of {total} exercises</b>
+    </div>
+  )
+}
+
+const Part = ({ part }) => <div>{part.name} {part.exercises}</div>
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header tittle={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
+
+  return courses.map(x => <Course key={x.id} course={x} />)
+
+
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
