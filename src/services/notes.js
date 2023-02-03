b37bb427f4ed99844676d0996b20ea9eae004c51
Giancarlo leandro <giancarlosleandro1@gmail.com>
2023-02-03 00:00:52 -0500

    Railway

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+//const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://node-notes.up.railway.app/api/notes'
 
 const getAll = () => {
     return axios.get(baseUrl).then(response => response.data)
